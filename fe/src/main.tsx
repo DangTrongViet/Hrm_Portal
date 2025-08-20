d14df509508fbba609e1da72cf4c40747a315278
Dang Trong Viet <viet113059@gmail.com>
2025-08-20 23:58:19 +0700

    hrm_portal: update fe

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-// src/main.tsx
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import AppRoutes from './app/routes';
 import './App.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
